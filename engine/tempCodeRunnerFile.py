baeca8976cba403f30fac95e823f6f5a3b20e22a
aditya2002r <aditya2002r@gmail.com>
2024-02-08 18:26:44 +0530

    second commit

---
+++ engine/tempCodeRunnerFile.py
@@ -0,0 +1,2 @@
+query = "CREATE TABLE IF NOT EXISTS sys_command(id integer primary key, name VARCHAR(100), path VARCHAR(1000))"
+# cursor.execute(query)
--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 import os
-
 import eel
 
-eel.init("www")
- 
-os.system('start msedge.exe --app="http://localhost:8000/index.html"')
+from engine.features import *
+from engine.command import *
 
-eel.start('index.html', mode=None , host='localhost' , block=True)
+def start():
+    eel.init("www")
+ 
+    playAssistantSound()
+
+    os.system('start msedge.exe --app="http://localhost:8000/index.html"')
+
+    eel.start('index.html', mode=None , host='localhost' , block=True)
+
